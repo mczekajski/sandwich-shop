3d59825e23db26de02066a620bf0e618a300bbd3
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-04-17 11:39:43 +0200

    prepare sandwich form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from "@angular/core";
     <div style="text-align:center" class="content">
       <h1>Welcome to {{ title }}!</h1>
     </div>
+    <wsb-sandwich-form></wsb-sandwich-form>
+
     <router-outlet></router-outlet>
   `,
   styles: [],
--- src/app/sandwich/sandwich-form/sandwich-form.component.ts
+++ src/app/sandwich/sandwich-form/sandwich-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Ingredient, Sauce } from '../sandwich';
 
 @Component({
   selector: 'wsb-sandwich-form',
@@ -7,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SandwichFormComponent implements OnInit {
+  public sandwichForm: FormGroup;
 
-  constructor() { }
+  public sauces = [
+    { label: 'No sauce', value: Sauce.None },
+    { label: 'Barbecue sauce', value: Sauce.Barbecue },
+    { label: 'Hot sauce', value: Sauce.Hot },
+    { label: 'Mayonnaise sauce', value: Sauce.Mayo }
+  ]
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { }
+
+  public ngOnInit(): void {
+    this.sandwichForm = this.formBuilder.group({
+      name: ['', Validators.minLength(3)],
+      ingredients: this.formBuilder.group({
+        [Ingredient.Lettuce]: false,
+        [Ingredient.Chorizo]: false,
+        [Ingredient.Ham]: false,
+        [Ingredient.Cheese]: false,
+        [Ingredient.Tomato]: false,
+      }),
+      sauce: Sauce.None,
+      vege: false,
+      price: 0
+    })
+  }
+
+  public save(): void {
+    const formValue = this.sandwichForm.getRawValue();
+
+    console.log(formValue);
   }
 
 }
