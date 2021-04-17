a6ae5c2089e10b3cd99dc49e69954c2f480693e6
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-04-17 09:34:56 +0200

    add sandwich interface

--- src/app/sandwich/sandwich.ts
+++ src/app/sandwich/sandwich.ts
@@ -1,7 +1,23 @@
+export enum Ingredient {
+    Lettuce = 'lettuce',
+    Chorizo = 'chorizo',
+    Ham = 'ham',
+    Cheese = 'cheese',
+    Tomato = 'tomato'
+}
+
+export enum Sauce {
+    None = 'none',
+    Barbecue = 'bbq',
+    Hot = 'hot',
+    Mayo = 'mayo'
+}
+
 export interface Sandwich {
+    id?: number;
     name: string;
-    ingredients;
-    sauce;
+    ingredients: Ingredient[];
+    sauce: Sauce;
     vege: boolean;
     price: number;
 }
